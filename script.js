3d3a83e4403696d66037f804851127a00e707901
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2025-06-13 22:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-document.getElementById('enterBtn').addEventListener('click', function() {
-	const status = document.getElementById('status');
-	status.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
-});
+ document.getElementById('enterBtn').addEventListener('click', function() {
+      document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
+    });
